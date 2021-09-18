6a34652cddbeb472d7b63640320c3c303316f3d1
mahmoodalobaidy01 <mahmood.rafid24@gmail.com>
2021-09-18 17:00:19 +0300

    q2

--- homeworkw2.js
+++ homeworkw2.js
@@ -16,3 +16,23 @@ return a;
 }
 let x =sortByLength(["a", "ccc", "dddd", "bb"]);
 console.log(x);
+drinks = [
+   {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+]
+function sortDrinkByPrice(a){
+    let swap = true;
+    while(swap){
+        swap = false;
+for(let i=0; i<a.length-1; i++){
+if(a[i].price > a[i+1].price){
+    let temp = a[i];
+    a[i] = a[i+1];
+    a[i+1] = temp;
+    swap = true;
+}
+}
+    }
+    return a;
+}
+console.log(sortDrinkByPrice(drinks))
